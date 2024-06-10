5f48a5942d7faf70df48669d2cdf2806c752b259
lynx <122455759+LynxSlash@users.noreply.github.com>
2024-06-10 17:16:11 +0500

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/matrixx_rosemary.mk
+    $(LOCAL_DIR)/lineage_rosemary.mk
 
 COMMON_LUNCH_CHOICES := \
-    matrixx_rosemary-eng \
-    matrixx_rosemary-userdebug \
-    matrixx_rosemary-user
+    lineage_rosemary-eng \
+    lineage_rosemary-userdebug \
+    lineage_rosemary-user
